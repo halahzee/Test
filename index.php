7fa537082cebd9c68e70f9796144e2e3ab98e0be
halaha <halahzee@gmail.com>
2023-12-05 20:49:16 -0600

    create a new branch

--- index.php
+++ index.php
@@ -3,4 +3,20 @@
 
 <h2>This is a Test Reop</h2>
 
+<ul>List of information i need to add here for gzwrite
+<li>Create a new branch</li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+
+
+
+</ul>
+
 ?>
