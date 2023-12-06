2e1b64518f76df17b93bb84c297df0d77245fdfb
halaha <halahzee@gmail.com>
2023-12-05 20:55:08 -0600

    update ul list

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <ul>List of information i need to add here for gzwrite
 <li>Create a new branch</li>
 <li>Useing the command (git push --set-upstream origin testBranch) to push the chnages to the new branch.</li>
-<li></li>
+<li>Trying to push changes to the main brach</li>
 <li></li>
 <li></li>
 <li></li>
