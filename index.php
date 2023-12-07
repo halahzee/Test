7ff641cf5e3a95479bf6331fbb9794be82b5f162
Halah Alazzawi <halaha@webspec.com>
2023-12-07 09:52:11 -0600

    update

--- index.php
+++ index.php
@@ -6,41 +6,4 @@
 ?>
 
 
-FEB 22		DUNGANNON DAIRY SALE 64
-			At Dungannon Mart, Co.Tyrone
-
-FEB 24		MILKING HERD DISPERSAL SALE
-			On farm Co.Meath
-
-MAR 2		GREENLAWN SALE
-			At Knockane, Brosna, Co.Kerry for The Curtin Family
-
-MAR 13 		CARNAROSS DAIRY SALE 150
-			At & in conj. with Carnaross Mart, Kells, Co.Meath
-
-MAR 21		DUNGANNON DAIRY SALE 65
-			At Dungannon Mart, Co.Tyrone
-
-APR 6		Reserved for Sale
-
-APR 10 		CARNAROSS DAIRY SALE 151
-			At & in conj. with Carnaross Mart, Kells, Co.Meath
-
-
-APR 17		Reserved for Sale
-
-
-APR 18		DUNGANNON DAIRY SALE 66
-			At Dungannon Mart, Co.Tyrone
-
-
-APR 20		Reserved for Sale
-
-
-
-MAY 8		CARNAROSS DAIRY SALE 152
-			At & in conj. with Carnaross Mart, Kells, Co.Meath
-
-
-MAY 22 		DUNGANNON DAIRY SALE 67
 			At Dungannon Mart, Co.Tyrone
